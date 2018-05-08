e296a2493cf1c0704d97fc4d41ea65393f77395e
Gui <33119610+GuiMarion@users.noreply.github.com>
2018-05-08 22:21:52 +0200

    CRT and modular exp

--- Euclide.py
+++ Euclide.py
@@ -1,20 +1,43 @@
 import sys
+from sympy import *
 
 
-def Euclide1(a, u0, v0, b, u1, v1):
+# Recursive Euclide, but not efficient with large numbers
+# def Euclide(a,b):
+
+# 	return Euclide1(a, 1, 0, b, 0, 1)
+
+# def Euclide1(a, u0, v0, b, u1, v1):
+# 	# return (x1, x2, x3)
+# 	# With a PGCD(a,b)
+# 	# x2 first bezout coef
+# 	# x3 second bezout coef
+# 	if b == 0:
+# 		return (a, u0, v0)
+# 	else:
+# 		q = int(a/b)
+# 		return (Euclide1(b, u1, v1, a -q*b, u0 - q*u1, v0 - q*v1)) 
+
+
+def Euclide(a, b):
 	# return (x1, x2, x3)
-	# With a PGCD(a,b)
+	# With x1 PGCD(a,b)
 	# x2 first bezout coef
 	# x3 second bezout coef
-	if b == 0:
-		return (a, u0, v0)
-	else:
+
+	(a, u0, v0, b, u1, v1) = (a, 1, 0, b, 0, 1)
+
+	while b != 0:
+		(a_pred, u0_pred, v0_pred, b_pred, u1_pred, v1_pred) = (a, u0, v0, b, u1, v1)
 		q = int(a/b)
-		return (Euclide1(b, u1, v1, a -q*b, u0 - q*u1, v0 - q*v1)) 
+		a = b_pred
+		u0 = u1_pred
+		v0 = v1_pred
+		b = a_pred - q*b_pred
+		u1 = u0_pred - q*u1_pred
+		v1 = v0_pred - q*v1_pred
 
-
-def Euclide(a,b):
-	return Euclide1(a, 1, 0, b, 0, 1)
+	return (a, u0, v0)
 
 
 
--- RSA.py
+++ RSA.py
@@ -1,8 +1,20 @@
 from sympy import *
 from random import *
 from Euclide import *
+import time
+
 
 class RSA:
+
+	def __init__(self):
+
+		self.dp = None
+		self.dq = None
+		self.qinv = None
+		self.p = None
+		self.q = None
+
+
 	def KeyGen(self, lambd):
 		lambd = int(lambd/2)
 		rangePrime = [2**(lambd-1), 2**(lambd)-1]
@@ -10,43 +22,108 @@ class RSA:
 		p = randprime(rangePrime[0], rangePrime[1])
 		q = randprime(rangePrime[0], rangePrime[1])
 
-		N = p*q
-
 		while (not isprime(p)):
 			p = randprime(rangePrime[0], rangePrime[1])
 
 		while (not isprime(q)):
 			q = randprime(rangePrime[0], rangePrime[1])
 
+
 		Phi = (p-1)*(q-1)
+
+		N = p*q
 
 		e = randint(1, Phi)
 
-		while (Euclide(e, Phi)[0] != 1):
+		# The primality with p-1 should be unecessery but for some reasons 
+		# if we don't check it the computation of the modinv of e, p-1 fails
+		while (Euclide(e, Phi)[0] != 1 or Euclide(e, p-1)[0] != 1):
 			e = randint(1, Phi)
 
 		d = Euclide(e, Phi)[1]
 		while d < 0:
 			d += Phi
 
+		self.dp = self.ModInv(e, p-1)
+		self.dq = self.ModInv(e, q-1)
+		self.qinv = self.ModInv(q, p)
+		self.p = p 
+		self.q = q
+
 		PublicKey = (e, N)
 		PrivateKey = (d, N)
 		return (PublicKey, PrivateKey)
+
+	def ExpBinMod(self, m,e,N):
+
+		e = list("{0:b}".format(e))
+		t = len(e)
+		b = 1
+		for i in range(t):
+			b = (b**2) % N
+			if (e[i] == '1'):
+				b = (b * m) % N
+
+		return b
 
 
 	def Encrypt(self, M, PublicKey):
 
 		(e, N) = PublicKey
-		c = Pow(M, e) % N
+		#c = Pow(M, e) % N
 
-		return c
+		return self.ExpBinMod(M, e, N)
 
 
 	def Decrypt(self, C, PrivateKey):
 
 		(d, N) = PrivateKey
 
-		return Pow(C, d) % N
+		return self.ExpBinMod(C, d, N)
+
+	def ModInv(self,a,m):
+
+		(pgcd, u0,_) = Euclide(a,m)
+
+		if pgcd != 1 :
+
+			raise ValueError("Unable to compute the ModInv bacause the gcd is not 1")
+
+
+		if u0 < 0 : 
+			return u0 + m
+		return u0
+
+
+
+	def CRT(self,a,m):
+		# a and m vector of size r
+		# x = ai mod mi for all i
+
+		M = 1
+
+		for elem in m:
+			M = M*elem
+
+		r = len(a)
+		if len(a) != len(m):
+			raise ValueError("Invalid input, a and m shoud have the same size.")
+		result = 0
+		for i in range(r):
+			Mi = int(M / m[i])
+			yi= self.ModInv(Mi,m[i])
+			result = result + a[i]*Mi*yi % M
+		return result % M
+
+	def Decrypt_CRT(self, C):
+
+		m1 = self.ExpBinMod(C, self.dp, self.p)
+		m2 = self.ExpBinMod(C, self.dq, self.q)
+		h = (self.qinv * (m1 - m2)) % self.p 
+		m = m2 + h * self.q
+
+		return m
+
 
 
 def Test(lambd, M, string = False):
@@ -59,11 +136,26 @@ def Test(lambd, M, string = False):
 	Crypt = R.Encrypt(M, PublicKey)
 	print("Encrypted message:", Crypt)
 
+	start_time = time.time()
 	Decrypt = R.Decrypt(Crypt, PrivateKey)
+	print("Naive : --- %s seconds ---" % (time.time() - start_time))
+
+	start_time = time.time()
+	Decrypt2 = R.Decrypt_CRT(Crypt)
+	print("CRT : --- %s seconds ---" % (time.time() - start_time))
+
 	if string == False:
 		print("Decrypted message:", Decrypt)
 	else : 
 		print("Decrypted message:", IntToString(Decrypt))
+
+	if string == False:
+		print("Decrypted message with CRT:", Decrypt2)
+	else : 
+		print("Decrypted message with CRT:", IntToString(Decrypt2))
+
+
+	#print(R.CRT([3,4,5],[17,11,6]))
 
 def StringToInt(M):
 	S = ""
@@ -78,14 +170,14 @@ def IntToString(M):
 
 if __name__ == "__main__":
 
-    if len(sys.argv) == 3:
-    	try :
-        	Test(int(sys.argv[1]), int(sys.argv[2]))
+	if len(sys.argv) == 3:
+		try :
+			Test(int(sys.argv[1]), int(sys.argv[2]))
 
-    	except ValueError:
-    		Test(int(sys.argv[1]), int(StringToInt(sys.argv[2])), string = True)
-    else:
-        print("Usage: Python3 RSA.py <lambda> <Message to encrypt>")
+		except ValueError:
+			Test(int(sys.argv[1]), int(StringToInt(sys.argv[2])), string = True)
+	else:
+		print("Usage: Python3 RSA.py <lambda> <Message to encrypt>")
 
 
 
